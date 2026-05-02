fa3e9f4349cd5ef5021f20acceeea51822a17a6d
jacks2005 <jacksimpsonkgss@gmail.com>
2026-05-01 20:50:20 -0700

    Finished peewee models

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from peewee import SqliteDatabase, Model, CharField, IntegerField, DateField
+from datetime import datetime
+from peewee import SqliteDatabase, Model, CharField, IntegerField, FloatField, DateField, ForeignKeyField
 
 db = SqliteDatabase("pantry.db")
 
@@ -16,6 +17,7 @@ class Ingredient(Model):
     class Meta:
         database = db
 
+
 class Food(Model):
     name = CharField()
     description = CharField(null=True)
@@ -31,7 +33,50 @@ class Food(Model):
         database = db
 
 
+class Recipe(Model):
+    title = CharField()
+    description = CharField(null=True)
+    prep_time_minutes = IntegerField()
+    cook_time_minutes = IntegerField()
+    servings = IntegerField()
+    tips = CharField(null=True)
+    created_at = DateField(default=datetime.now)
+
+    class Meta:
+        database = db
+
+
+class RecipeIngredient(Model):
+    recipe = ForeignKeyField(Recipe, backref="ingredients")
+    item = CharField()
+    amount = CharField()
+    unit = CharField(null=True)
+    preparation = CharField(null=True)
+
+    class Meta:
+        database = db
+
+
+class RecipeMissingIngredient(Model):
+    recipe = ForeignKeyField(Recipe, backref="missing_ingredients")
+    item = CharField()
+    amount = CharField()
+    unit = CharField(null=True)
+    substitute = CharField(null=True)
+
+    class Meta:
+        database = db
+
+
+class RecipeInstruction(Model):
+    recipe = ForeignKeyField(Recipe, backref="instructions")
+    step_number = IntegerField()
+    instruction = CharField()
+
+    class Meta:
+        database = db
+
 
 def init_db():
     with db:
-        db.create_tables([Ingredient])
+        db.create_tables([Ingredient, Food, Recipe, RecipeIngredient, RecipeMissingIngredient, RecipeInstruction])
