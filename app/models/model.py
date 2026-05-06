24d030ac62d5d50e3c791310c2dbc8a6991db142
jacks2005 <jacksimpsonkgss@gmail.com>
2026-05-06 10:38:48 -0700

    implemented peewee base model

--- app/models/model.py
+++ app/models/model.py
@@ -2,27 +2,26 @@ from datetime import datetime
 from peewee import Model, CharField, IntegerField, FloatField, DateField, ForeignKeyField, AutoField
 from app.extensions.extensions import db
 
+
 class BaseModel(Model):
+    id = AutoField()
+    created_at = DateField(default=datetime.now)
+
     class Meta:
         database = db
 
-class Ingredient(Model):
-    id = AutoField()
+
+class Ingredient(BaseModel):
     name = CharField()
     quantity = FloatField()
     unit = CharField()
     category = CharField()
     expiry_date = DateField(null=True)
     notes = CharField(null=True)
-    created_at = DateField(default=datetime.now)
     updated_at = DateField(default=datetime.now)
 
-    class Meta:
-        database = db
 
-
-class Food(Model):
-    id = AutoField()
+class Food(BaseModel):
     name = CharField()
     description = CharField(null=True)
     food_type = CharField()
@@ -30,59 +29,38 @@ class Food(Model):
     category = CharField()
     expiry_date = DateField(null=True)
     notes = CharField(null=True)
-    created_at = DateField(default=datetime.now)
     updated_at = DateField(default=datetime.now)
 
-    class Meta:
-        database = db
 
-
-class Recipe(Model):
-    id = AutoField()
+class Recipe(BaseModel):
     title = CharField()
     description = CharField(null=True)
     prep_time_minutes = IntegerField()
     cook_time_minutes = IntegerField()
     servings = IntegerField()
     tips = CharField(null=True)
-    created_at = DateField(default=datetime.now)
-
-    class Meta:
-        database = db
 
 
-class RecipeIngredient(Model):
-    id = AutoField()
+class RecipeIngredient(BaseModel):
     recipe = ForeignKeyField(Recipe, backref="ingredients")
     item = CharField()
     amount = CharField()
     unit = CharField(null=True)
     preparation = CharField(null=True)
 
-    class Meta:
-        database = db
 
-
-class RecipeMissingIngredient(Model):
-    id = AutoField()
+class RecipeMissingIngredient(BaseModel):
     recipe = ForeignKeyField(Recipe, backref="missing_ingredients")
     item = CharField()
     amount = CharField()
     unit = CharField(null=True)
     substitute = CharField(null=True)
 
-    class Meta:
-        database = db
 
-
-class RecipeInstruction(Model):
-    id = AutoField()
+class RecipeInstruction(BaseModel):
     recipe = ForeignKeyField(Recipe, backref="instructions")
     step_number = IntegerField()
     instruction = CharField()
-
-    class Meta:
-        database = db
 
 
 def init_db():
