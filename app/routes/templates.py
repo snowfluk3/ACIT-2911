87a56b5bd7d168a7c734460473deafa16053f497
jacks2005 <jacksimpsonkgss@gmail.com>
2026-05-07 19:34:01 -0700

    Added back dashboard

--- app/routes/templates.py
+++ app/routes/templates.py
@@ -2,7 +2,10 @@ from flask import Blueprint, render_template
 
 template_bp = Blueprint("templates", __name__)
 
-"""Render HTML Pages"""
 @template_bp.route("/")
 def index():
     return render_template("index.html")
+
+@template_bp.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
